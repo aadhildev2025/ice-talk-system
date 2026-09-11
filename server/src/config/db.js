f431d2d3f3519098d1d618cbfb2edaa04507edf6
Aadhil <aadhildev2025@gmail.com>
2026-09-11 14:39:43 +0530

    Optimize configuration for Vercel deployment

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
   try {
     const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/icetalk_restaurant';
     const conn = await mongoose.connect(mongoURI, {
